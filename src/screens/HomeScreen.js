fca8ea35058e9320ae132a2bd58a8db919d074a1
Matthew Bates <matthewhcbates@gmail.com>
2023-01-10 18:18:47 -0700

    added <Button/> && <TouchableOpacity> functionality

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Button, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+// there are TWO types of buttons with React Native: <Button/> and <TouchableOpacity>
+// the <Button /> element is self-closing, <TouchableOpacity> is not
+
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        title="Go to Components screen"
+        onPress={() => props.navigation.navigate("Components")}
+      />
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text>Go to list Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,27 +3,35 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", age: 20 },
+    { name: "Friend #2", age: 45 },
+    { name: "Friend #3", age: 32 },
+    { name: "Friend #4", age: 27 },
+    { name: "Friend #5", age: 53 },
+    { name: "Friend #6", age: 30 },
+    { name: "Friend #7", age: 22 },
+    { name: "Friend #8", age: 28 },
+    { name: "Friend #9", age: 35 },
   ];
 
   return (
     <View>
       <FlatList
+        // horizontal
+        // showsHorizontalScrollIndicator={false}
         data={friends}
         // destructure off the item property from friends array
         renderItem={({ item }) => {
-          return <Text style={styles.textStyle}>{item.name}</Text>;
+          return (
+            <View>
+              <Text style={styles.textStyle}>
+                {item.name} - Age {item.age}
+              </Text>
+            </View>
+          );
         }}
-        keyExtractor={(friends) => {
-          friends.name;
+        keyExtractor={(friend) => {
+          friend.name;
         }}
       />
     </View>
